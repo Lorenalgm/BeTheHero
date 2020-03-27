3cc5cd87fd1990066f12adcaa7956f2f00b329b1
Lorenalgm <lorena@proesc.com>
2020-03-27 20:28:27 -0300

    Criada lógica para registro de ongs e incidentes

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,21 @@
 const express = require('express');
 
+const OngController = require('./controllers/OngController');
+const IncidentsController = require('./controllers/IncidentsController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
-    
-    return response.json({
-        evento: 'Semana Omnistack',
-        aluno: 'Lorena'
-    });
-});
+routes.post('/sessions', SessionController.store);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentsController.index);
+routes.post('/incidents', IncidentsController.create);
+routes.delete('/incidents/:id', IncidentsController.delete);
 
 module.exports = routes;
