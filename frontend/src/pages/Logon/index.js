dc4064f5163a49dcfa04e3460dc7cfc984ceb3ed
Lorenalgm <lorena@proesc.com>
2020-04-07 20:08:04 -0300

    Criada estrutura do Register

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.css';
 import logoImg from '../../assets/logo.svg'
 import hearoesImg from '../../assets/heroes.png'
@@ -14,10 +15,10 @@ export default function Logon() {
                     <h1>Faça seu login</h1>
                     <input placeholder="Sua ID" />
                     <button className="button" type="submit">Entrar</button>
-                    <a href="/register">
+                    <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041" />
                         Não tenho cadastro
-                    </a>
+                    </Link>
                 </form>
             </section>
 
