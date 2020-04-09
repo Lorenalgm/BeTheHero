951d8b7497060ba78b4534290401e141a79210c2
Lorenalgm <lorena@proesc.com>
2020-04-08 23:08:35 -0300

    Conectado com backend

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import './styles.css';
+import api from '../../services/api';
 import logoImg from '../../assets/logo.svg'
 import hearoesImg from '../../assets/heroes.png'
 import { FiLogIn } from 'react-icons/fi'
 
 export default function Logon() {
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e) {
+        e.preventDefault();
+        
+        try{
+            const response = await api.post('sessions', { id });
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('profile');
+        } catch(err){
+            alert('Falha no login, tente novamente!');
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Be the hero" />
 
-                <form>
+                <form onSubmit={handleLogin}>
                     <h1>Faça seu login</h1>
-                    <input placeholder="Sua ID" />
+                    <input 
+                        placeholder="Sua ID"
+                        value={id}
+                        onChange={e => setId(e.target.value)}
+                    />
                     <button className="button" type="submit">Entrar</button>
                     <Link className="back-link" to="/register">
                         <FiLogIn size={16} color="#E02041" />
--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState} from 'react';
+import {Link, useHistory} from 'react-router-dom';
 import {FiArrowLeft} from 'react-icons/fi';
 import './styles.css';
 import logoImg from '../../assets/logo.svg'
+import api from '../../services/api';
 
 export default function NewIncident() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+    const history = useHistory();
+    const ongId = localStorage.getItem('ongId');
+
+    async function handleNewIncident(e){
+        e.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value
+        };
+        
+        try{
+            await api.post('incidents', data, {
+                headers: {
+                    Authorization: ongId,
+                }
+            });
+
+            history.push('/profile');
+        }catch(err){
+            alert('Erro ao cadastrar caso, tente novamente')
+        }
+    }
+
     return (
         <div className="new-incident-container">
         <div className="content">
@@ -17,10 +46,19 @@ export default function NewIncident() {
                     Voltar para home
                 </Link>
             </section>
-            <form>
-                <input placeholder="Título do caso" />
-                <textarea placeholder="Descrição" />
-                <input placeholder="Valor em reais" />
+            <form onSubmit={handleNewIncident}>
+                <input 
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                    placeholder="Título do caso" />
+                <textarea 
+                    value={description}
+                    onChange={e => setDescription(e.target.value)}
+                    placeholder="Descrição" />
+                <input 
+                    value={value}
+                    onChange={e => setValue(e.target.value)}
+                    placeholder="Valor em reais" />
 
                 <button type="submit" className="button">
                     Cadastrar
